2a18935b155708a711ca06f3894a1a8e901d737d
yanlele <331393627@qq.com>
2019-12-28 18:57:58 +0800

    revert(remove): test.js

--- test/math.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.sampleFunction = str => {
-  return str + str;
-};
-//# sourceMappingURL=math.js.map
--- test/math.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const math_1 = require('./math');
-describe('main', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(math_1.sampleFunction('hello')).toBe('hellohello');
-  });
-});
-//# sourceMappingURL=math.test.js.map
