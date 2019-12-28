4fc0410f7cdedd7747a794a56e407736800c9a71
yanlele <331393627@qq.com>
2019-12-28 18:36:22 +0800

    feat(test): add test demo

---
+++ test/math.test.ts
@@ -0,0 +1,7 @@
+import { sampleFunction } from './math';
+
+describe('main', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sampleFunction('hello')).toBe('hellohello');
+  });
+});
---
+++ test/math.ts
@@ -0,0 +1,3 @@
+export const sampleFunction = (str: string) => {
+  return str + str;
+};
