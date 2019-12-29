055a2959a7d017194e1910a445c77dbea1f10e8a
yanlele <331393627@qq.com>
2019-12-30 00:09:58 +0800

    feat(core): modify replace

--- src/core/handle.ts
+++ src/core/handle.ts
@@ -15,12 +15,12 @@ export const readMDFiles = (): void => {
   const paths = readMarkDownFilePath();
   const contentList = paths.map(item => fse.readFileSync(item, { encoding: 'utf8' }));
   const content = contentList[0]
-    .replace(/\n#####\s(.*)/g, '\n### <h5 id="$1">$1</h5>')
-    .replace(/\n####\s(.*)/g, '\n### <h4 id="$1">$1</h4>')
-    .replace(/\n###\s(.*)/g, '\n### <h3 id="$1">$1</h3>')
-    .replace(/\n##\s(.*)/g, '\n## <h2 id="$1">$1</h2>')
-    .replace(/^#\s(.*)/g, '# <h1 id="$1">$1</h1>')
-    .replace(/^##\s(.*)/g, '## <h1 id="$1">$1</h1>');
+    .replace(/\n#####\s(.*)/g, '\n<h5 id="$1">$1</h5>')
+    .replace(/\n####\s(.*)/g, '\n<h4 id="$1">$1</h4>')
+    .replace(/\n###\s(.*)/g, '\n<h3 id="$1">$1</h3>')
+    .replace(/\n##\s(.*)/g, '\n<h2 id="$1">$1</h2>')
+    .replace(/^#\s(.*)/g, '<h1 id="$1">$1</h1>')
+    .replace(/^##\s(.*)/g, '<h1 id="$1">$1</h1>');
   console.log(content);
   // console.log(content[0].match(/^##\s.*/m));
   // content[0].match(/###\s.*/g).forEach(item => {
